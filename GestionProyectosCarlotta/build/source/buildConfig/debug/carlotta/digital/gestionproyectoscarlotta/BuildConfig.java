2b825ab6b1bd8518efd5f2cc8361cbceb7c87858
Borja José Celda Gonzalez <programacion@carlottadigital.com>
2013-06-24 13:14:53 +0200

    Primer commit (Proyecto vacio)

---
+++ GestionProyectosCarlotta/build/source/buildConfig/debug/carlotta/digital/gestionproyectoscarlotta/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package carlotta.digital.gestionproyectoscarlotta;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
