d695dd8863e9e0e15850338016b6709dc9c4c825
Borja José Celda Gonzalez <programacion@carlottadigital.com>
2013-06-24 13:39:37 +0200

    Test commit 2

--- GestionProyectosCarlotta/src/main/java/carlotta/digital/gestionproyectoscarlotta/MainActivity.java
+++ GestionProyectosCarlotta/src/main/java/carlotta/digital/gestionproyectoscarlotta/MainActivity.java
@@ -1,16 +1,24 @@
 package carlotta.digital.gestionproyectoscarlotta;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
 
 public class MainActivity extends Activity {
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_main);
-        System.out.println("asd");
+        //Inicializar ActionBar
+        initActionBar();
     }
 
 
@@ -20,5 +28,8 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    public void initActionBar(){
+        actionBar = getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FE0000")));
+    }
 }
